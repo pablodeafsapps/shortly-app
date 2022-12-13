20289e270b981bbe1a4dde1bc4b2e2d1e47551ec
Pablo L. Sordo <pablodeafsapps@gmail.com>
2022-12-13 13:00:04 +0100

    fix: amend package name for this project

---
+++ application/app/src/main/java/org/deafsapps/shortlyapp/urlshortening/domain/model/UrlShorteningBo.kt
@@ -0,0 +1,18 @@
+package org.deafsapps.shortlyapp.urlshortening.domain.model
+
+@JvmInline
+value class Url(val value: String)
+
+data class ShortenUrlOperationBo(
+    val status: ShortenUrlOpStatusBo,
+    val result: ShortenUrlOpResultBo
+)
+
+@JvmInline
+value class ShortenUrlOpStatusBo(val isSuccessful: Boolean)
+
+data class ShortenUrlOpResultBo(
+    val code: String,
+    val shortLink: String,
+    val fullShortLink: String
+)
--- application/app/src/main/java/org/pablodeafsapps/shortlyapp/urlshortening/domain/utils/ExtensionUtils.kt
+++
@@ -1,5 +0,0 @@
-package org.pablodeafsapps.shortlyapp.urlshortening.domain.utils
-
-import org.pablodeafsapps.shortlyapp.urlshortening.domain.model.Url
-
-fun Url.isValid(): Boolean = value.isNotEmpty()
